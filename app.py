f788d5ac1f350727cbf71ccc432e61ca55e53633
Justin Chung <jjkchung@gmail.com>
2023-06-18 17:35:13 -0400

    remaining questions

--- app.py
+++ app.py
@@ -1,48 +1,84 @@
-from flask import Flask, render_template, request, redirect, flash
+from flask import Flask, render_template, request, redirect, flash, session
 from flask_debugtoolbar import DebugToolbarExtension
-from surveys import satisfaction_survey as survey
+from surveys import surveys
+
+
 app = Flask(__name__)
-app.config['SECRET_KEY'] = '12345'
-app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
+app.config['SECRET_KEY'] = '12345fdshjfjkdshf'
+# app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
 
 debug = DebugToolbarExtension(app)
 
-responses = []
 
 @app.route('/')
-def presentAndStartSurvey():
-    """presents and starts survey"""
-    title = survey.title
-    instructions = survey.instructions
-
-    return render_template('home.html',title=title,instructions=instructions)
-
-
-@app.route('/question/<int:number>')
-def showQuestion(number):
-    
-    if number > len(responses):
-        flash('Have not completed earlier questions. Please complete questions in order.')
-        return redirect(f'/question/{len(responses)}')
-        
- 
+def presentSurveyOptions():
+    """presents survey options"""
+    surveyNames= surveys.keys()
+    if 'responses' not in session:
+        session['responses'] = {}
+        return render_template('home.html',surveys=surveyNames)
     else:
-        questions = survey.questions
+        return render_template('home.html',surveys=surveyNames)
+    
+
+@app.route('/', methods=['POST'])
+def presentSurveyIntro():
+    """"""
+    selectedSurvey = request.form['selected-survey']
+    if selectedSurvey in session['responses']:
+        return redirect(f'/survey/{selectedSurvey}')
+    else:
+        session['responses'][selectedSurvey] = []
+        return redirect(f'/survey/{selectedSurvey}')
+
+
+@app.route('/survey/<survey>')
+def presentSurveyInstructions(survey):
+    
+    title = surveys[survey].title
+    instructions = surveys[survey].instructions
+    return render_template('survey-start.html',title=title,instructions=instructions,survey=survey)
+
+
+@app.route('/survey/<survey>/question/<int:number>')
+def showQuestion(survey, number):
+    
+    if len(session['responses'][survey]) == len(surveys[survey].questions):
+        return redirect(f'/survey/{survey}/thank-you')
+    
+    if number > len(session['responses'][survey]):
+        flash('Have not completed earlier questions. Please complete questions in order.')
+        return redirect(f"/survey/{survey}/question/{len(session['responses'][survey])}")
+           
+    else:
+        questions = surveys[survey].questions
         q = questions[number].question
         choices = questions[number].choices
-        return render_template('questions.html',question=q,choices=choices)
+        allowText= questions[number].allow_text
+        return render_template('questions.html',question=q,choices=choices,allowText=allowText)
+        
 
-@app.route('/question/<int:number>',methods=['POST'])
-def handleResponse (number):
+@app.route('/survey/<survey>/question/<int:number>',methods=['POST'])
+def handleResponse (survey, number):
     choice = request.form['selected_option']
-    responses.append(choice)
-    if len(responses) == len(survey.questions):
+    question = surveys[survey].questions[number].question
+
+    if survey in session['responses']:
+        responses = session['responses'][survey]
+        responses.append(f'<b>{question}:</b> {choice}')
+        session['responses'][survey] = responses
+    else:
+        session['responses'][survey] = [f'<b>{question}:</b> {choice}']
+
+
+    if len(session['responses'][survey]) == len(surveys[survey].questions):
         return redirect('/thank-you')
     else:
-        return redirect(f'/question/{number+1}')
+        return redirect(f"/survey/{survey}/question/{len(session['responses'][survey])}")
 
 
 
-@app.route('/thank-you')
-def showThankYou():
-    return render_template('thank-you.html')
+@app.route('/<survey>/thank-you')
+def showThankYou(survey):
+    responses = session['responses'][survey]
+    return render_template('thank-you.html',survey=survey,responses=responses)
